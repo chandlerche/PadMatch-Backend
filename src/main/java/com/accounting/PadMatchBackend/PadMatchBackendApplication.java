985740dc0ccd8e2638b852a5c176222b5d9bee20
Jianhua Che <che.ji@northeastern.edu>
2024-02-22 16:08:23 -0500

    first commit

---
+++ src/main/java/com/accounting/PadMatchBackend/PadMatchBackendApplication.java
@@ -0,0 +1,13 @@
+package com.accounting.PadMatchBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PadMatchBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PadMatchBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/accounting/PadMatchBackend/PadMatchBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accounting.PadMatchBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PadMatchBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
