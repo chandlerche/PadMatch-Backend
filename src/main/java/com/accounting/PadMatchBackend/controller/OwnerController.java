72594f3f223b3cb2f9c676cf6c8a383d9a0265e4
Jianhua Che <che.ji@northeastern.edu>
2024-02-22 20:55:32 -0500

    feat: add post new owner method

--- src/main/java/com/accounting/PadMatchBackend/controller/OwnerController.java
+++ src/main/java/com/accounting/PadMatchBackend/controller/OwnerController.java
@@ -1,4 +1,25 @@
 package com.accounting.PadMatchBackend.controller;
 
+import com.accounting.PadMatchBackend.model.Owner;
+import com.accounting.PadMatchBackend.repository.OwnerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class OwnerController {
+
+    @Autowired
+    private OwnerRepository ownerRepository;
+
+    @PostMapping("/owner")
+    Owner newOwner(@RequestBody Owner newOwner) {
+        return ownerRepository.save(newOwner);
+    }
+
+
 }
